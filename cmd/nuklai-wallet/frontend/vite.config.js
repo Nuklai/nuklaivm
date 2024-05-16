8175619b24ec2a4d4eb56fd6d5357081e6b980a3
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-05-16 12:09:51 -0400

    Moved nuklai faucet, feed and wallet to their own repo

--- cmd/nuklai-wallet/frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2024, AllianceBlock. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  plugins: [react()]
-})
